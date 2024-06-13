f50bb15c6568f85d204e7b063511deac4756af1e
unknown <noveraalam2@gmail.com>
2024-06-13 18:47:15 +0200

    started login form

--- src/app/novera-project/novera-project.module.ts
+++ src/app/novera-project/novera-project.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../pages/login/login.component';
 
-import { NoveraProjectRoutingModule } from './novera-project-routing.module';
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    NoveraProjectRoutingModule,
     LoginComponent
   ]
 })
